2d75f73fab7732af4a5fd3760d717c8422ad7c35
Maken Hubbard <makhub26@outlook.com>
2018-03-31 12:42:34 -0500

    I have the show image and disappearing image working

--- main.js
+++ main.js
@@ -17,17 +17,46 @@ const buildDomString = (planetArray) => {
     planetArray.forEach((planet) => {
         domString += `<div class= "cards">
                         <h2>${planet.name}</h2>
+                        <img class="hide" src="${planet.imageUrl}">
                       </div>`;
         
     })
     printToDom(domString,"main-cards");
 }
 
+const buildSearchBar = () => {
+
+}
+
 function executeThisIfXHRFails () {
     console.log("error");
-}
+}   
 
 function executeCodeAfterFileLoad () {
     const data = JSON.parse(this.responseText);
     buildDomString(data.planets);
+    addImageEventListener();
+}
+
+
+const addImageEventListener = () => {
+    const imageHover = document.getElementsByClassName('cards');
+    for (let i=0; i<imageHover.length; i++) {
+        imageHover[i].addEventListener('mouseenter',showImage);
+        imageHover[i].addEventListener('mouseleave',hideImage);
+    }   
+}
+
+const showImage = (e) => {
+    const displayImage = e.target.children[1];
+        displayImage.classList.remove('hide');
+    const displayName = e.target.children[0];
+        displayName.classList.add('hide');
+}
+
+const hideImage = (e) => {
+    const displayImage = e.target.children[1];
+    displayImage.classList.add('hide');
+const displayName = e.target.children[0];
+    displayName.classList.remove('hide');
 }
